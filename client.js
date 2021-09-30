2e7f2461d1061062ceb79648447960cc23cc65c7
FaisaFF <triplef07@gmail.com>
2021-09-30 15:54:28 +0700

    first commit

---
+++ client.js
@@ -0,0 +1,22 @@
+var mqtt = require("mqtt");
+// var client = mqtt.connect("mqtt://localhost:16517");
+// var client = mqtt.connect("mqtt://44.198.112.35:13217");
+var client = mqtt.connect("mqtt://44.198.112.35:13217");
+
+client.on("connect", function () {
+  client.subscribe("test");
+  var paket = {
+    temp: (Math.random() * 100).toFixed(2),
+    ppg: (Math.random() * 100).toFixed(2),
+    ekg: (Math.random() * 100).toFixed(2),
+    nivac: (Math.random() * 100).toFixed(2),
+    listAlatId: 1,
+  };
+  client.publish("test", JSON.stringify(paket));
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+});
